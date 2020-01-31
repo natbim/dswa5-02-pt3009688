08e0077ac0a8afe98abd03ab09ab8f189b68d3a6
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2020-01-31 13:25:54 -0300

    Criar repositório no GitHub

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,7 +2,7 @@ module.exports = function () {
 	var controller = {};
 	controller.index = function(req, res) {
 		//Retorna a página index.ejs
-		res.render('index', {nome: 'Express'});
+		res.render('index', {nome: 'ExpressJS'});
 	};
 	return controller;
 }
