7e36b3bcfd543e80ab7b06fcf86d1310541ea852
nathalia fonseca bim <nathaliafonsecabim15@gmail.com>
2022-03-31 20:29:01 -0300

    commit inicial

--- config/express.js
+++ config/express.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var home = require('../app/routes/home');
 module.exports = function(){
-	var app = express();	
-	app.set('port', 3000);
+	var app = express();
+	//Mudança no parâmetro PORT para publicar no Heroku
+	app.set('port', process.env.PORT || 3000);
 	//Middleware
 	app.use(express.static('./public'));
 	app.set('view engine', 'ejs');
